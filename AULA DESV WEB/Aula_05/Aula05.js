0fcd7b3f4bc8858cfcefe29cbb5e06aba9f82142
JonathanHF <jonathanhf08022001@gmail.com>
2025-06-19 12:07:49 -0300

    Aula 3 da DIO e subindo aulas antigas

---
+++ AULA DESV WEB/Aula_05/Aula05.js
@@ -0,0 +1,36 @@
+// solicitar o raio ao usuario;
+//var raio = prompt("Insira o raio do circulo: ");
+
+//converter para numero;
+//raio = parseFloat(raio);
+
+//calcular area do circulo;
+//const area = Math.PI*Math.pow(raio,2);
+
+//const area: cria uma constante chamada area para armazenar o resultado.
+//Math.PI: representa o valor de π (pi), aproximadamente 3.14159.
+//Math.pow(raio, 2): eleva o valor de raio ao quadrado (ou seja, raio²).
+
+//exibir resultado;
+//console.log("A area do circulo é: ",area);
+//alert("A area do circulo é: "+area.toFixed(2)); //toFixed() serve para delimitar as casas decimais
+
+
+// ------------------------Exemplo 2: --------------------------------------------
+
+//Solicita o sexo do usuario:
+
+//var sexo = prompt("Insira seu sexo com M para Masculino e F para Feminino");
+//var sexo = sexo.toUpperCase(); //converte a letra para maiusculo.
+//validar informação:
+
+//if (sexo === "F"){
+//    alert("Sexo feminino");
+//}
+//    else if (sexo === "M"){
+//        alert("Sexo masculino");
+//}
+//    else{alert (sexo invalido);
+//}
+
+//---------------------------------------- Exemplo 3:-----------------------------------------------------
